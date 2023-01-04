2d55e510291bac993e36b4523929f492c8fe6aa2
Phat <phatdroid94@gmail.com>
2023-01-04 19:30:17 +0700

    database

--- app/src/main/java/com/example/roomdatabase12102022/data/local/UserDatabase.java
+++ app/src/main/java/com/example/roomdatabase12102022/data/local/UserDatabase.java
@@ -1,7 +1,15 @@
 package com.example.roomdatabase12102022.data.local;
 
+import androidx.room.Database;
+
+import com.example.roomdatabase12102022.data.local.entities.HistoryEntity;
+import com.example.roomdatabase12102022.data.local.entities.UserEntity;
+
 /**
  * Created by pphat on 1/4/2023.
  */
-public class UserDatabase {
+
+@Database(entities = {UserEntity.class, HistoryEntity.class}, version = 1, exportSchema = false)
+public abstract class UserDatabase {
+
 }
--- app/src/main/java/com/example/roomdatabase12102022/data/local/dao/UserDAO.java
+++ app/src/main/java/com/example/roomdatabase12102022/data/local/dao/UserDAO.java
@@ -1,7 +1,11 @@
 package com.example.roomdatabase12102022.data.local.dao;
 
+import androidx.room.Dao;
+
 /**
  * Created by pphat on 1/4/2023.
  */
+
+@Dao
 public interface UserDAO {
 }
