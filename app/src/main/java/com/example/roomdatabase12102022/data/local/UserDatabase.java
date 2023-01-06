29cc16bbed6205604a5bf094fb9d25f29286966b
Phat <phatdroid94@gmail.com>
2023-01-06 18:07:39 +0700

    database

--- app/src/main/java/com/example/roomdatabase12102022/data/local/UserDatabase.java
+++ app/src/main/java/com/example/roomdatabase12102022/data/local/UserDatabase.java
@@ -1,7 +1,12 @@
 package com.example.roomdatabase12102022.data.local;
 
-import androidx.room.Database;
+import android.content.Context;
 
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.roomdatabase12102022.data.local.dao.UserDAO;
 import com.example.roomdatabase12102022.data.local.entities.HistoryEntity;
 import com.example.roomdatabase12102022.data.local.entities.UserEntity;
 
@@ -10,6 +15,18 @@ import com.example.roomdatabase12102022.data.local.entities.UserEntity;
  */
 
 @Database(entities = {UserEntity.class, HistoryEntity.class}, version = 1, exportSchema = false)
-public abstract class UserDatabase {
+public abstract class UserDatabase extends RoomDatabase{
+    public abstract UserDAO userDao();
+    private static UserDatabase instance;
 
+    public synchronized static UserDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(
+              context,
+              UserDatabase.class,
+              "my-user"
+            ).build();
+        }
+        return instance;
+    }
 }
